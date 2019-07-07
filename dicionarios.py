4d584af7fc9ba0f9750afa3b1c83f30ef1a32220
Cassio <cassio8186@gmail.com>
2019-07-07 14:08:09 -0300

    Desafio 92 guanabara feito.

--- dicionarios.py
+++ dicionarios.py
@@ -9,6 +9,20 @@ def pratica():
         print("e sua situação é reprovada")
 def desafio92():
     func=dict()
+    nascimento = 0
     func['nome']=input("Insira o nome")
-    func['nasci']=input("insira o ano de nascimento")
-    func['']
+    while nascimento < 1900:
+        nascimento = int(input("insira o ano de nascimento"))
+    func['idade'] = 2019 - nascimento
+    func['CTPS'] = int(input("Insira a CTPS"))
+    if func['CTPS'] != 0:
+        func['anocontratação'] = int(input("Insira o ano de contratação"))
+        func['salario'] = input("Insira o salário")
+        func['aposentadoria'] = 35 - (2019 - func['anocontratação'])
+        if func['aposentadoria'] <= 0:
+            func['aposentadoria'] = 'Aposentado'
+    print(f'O nome é {func["nome"]}. o Ano de nascimento é {func["idade"]}')
+    print(f'CTPS tem valor de {func["CTPS"]}')
+    if func['CTPS'] != 0:
+        print(f'Contratação tem valor de: {func["anocontratação"]}')
+        print(f"O salário tem valor de {func['salario']}. Aposentadoria tem valor de {func['aposentadoria']}")
